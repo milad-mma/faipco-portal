2a244637b25d07fbee39cb300ba0dd4350be3982
Milad Mohammad Alizadeh <milad.alizadeh01@gmail.com>
2026-08-01 09:31:14 +0330

    test

--- frontend/src/api/employees.js
+++
@@ -1,8 +0,0 @@
-import { apiClient } from "./client";
-
-export async function fetchEmployees({ siteId } = {}) {
-  const { data } = await apiClient.get("/employees", {
-    params: siteId ? { site_id: siteId } : {},
-  });
-  return data;
-}
